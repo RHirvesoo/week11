c729e50a6046340fd0b46a3a2d7a4b2e3b35f21c
Rasmus <rasmus.hirvesoo@tktk.ee>
2023-11-21 18:44:44 +0200

    week11 kood

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const { Pool } = require('pg');
-const itemsPool = new Pool({
-    user: 'postgres',
-    password: '12345678',
-    host: 'localhost',
-    port: 3000,
-    database: 'myrecipes'
+
+const itemsPool = new Pool ({
+
+    connectionString: process.env.DBConnectionString,
+    ssl: {
+        rejectauthorized: false
+    }
+
 });
+
 
 module.exports = itemsPool;
